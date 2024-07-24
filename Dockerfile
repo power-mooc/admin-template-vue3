668cec60f24c971b7f11bba8c1ea4477a1fd1f23
power <1410837981@qq.com>
2024-07-24 22:48:15 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# production stage
+FROM nginx 
+
+COPY dist/* /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>home</div>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <Icon icon="mdi:home" class="text-sky text-3xl"></Icon>
+</template>
+
+<script setup lang="ts">
+import { registerSW } from 'virtual:pwa-register'
+import { Icon } from '@iconify/vue'
+
+onMounted(() => {
+  registerSW({
+    immediate: true,
+    onRegisteredSW(_url, registration) {
+      console.log('🚀 ~ onRegisteredSW ~ registration:', registration)
+      console.log('🚀 ~ onRegisteredSW ~ _url:', _url)
+      setInterval(() => {
+        registration && registration.update()
+      }, 3600000)
+    }
+  })
+})
+</script>
+
+<style scoped></style>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
+
+function onMounted(arg0: () => void) { throw new Error('Function not implemented.') }
